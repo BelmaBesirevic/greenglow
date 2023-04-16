5be5b6fd6e0202f5019a1f21d580a8df244c548f
Belma Besirevic <72704864+belmab@users.noreply.github.com>
2023-04-16 23:45:46 +0200

    add contact

--- pages/contact.js
+++ pages/contact.js
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import axios from 'axios'
 
 export default function Contact() {
   const [inputs, setInputs] = useState({
@@ -21,12 +20,18 @@ export default function Contact() {
   const onSubmitForm = async (e) => {
     e.preventDefault()
 
-    if (inputs.name && inputs.email && inputs.subject && inputs.message) {
+    if (inputs.name && inputs.email && inputs.message) {
       setForm({ state: 'loading' })
       try {
-        const res = await axios.post('/api/contact', inputs)
+        const res = await fetch(`api/contact`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(inputs),
+        })
 
-        const { error } = await res.data
+        const { error } = await res.json()
 
         if (error) {
           setForm({
@@ -54,94 +59,51 @@ export default function Contact() {
       }
     }
   }
-
   return (
-    <div className='flex flex-col items-center py-8'>
-      <h2 className='text-2xl font-bold mb-4'>Contact Us</h2>
-      <form className='w-full max-w-lg' onSubmit={(e) => onSubmitForm(e)}>
-        <div className='flex flex-wrap -mx-3 mb-4'>
-          <div className='w-full px-3 mb-6'>
-            <label
-              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              htmlFor='name'
-            >
-              Name
-            </label>
-            <input
-              className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-              id='name'
-              type='text'
-              value={inputs.name}
-              onChange={handleChange}
-              placeholder='Name'
-              required
-            />
-          </div>
-          <div className='w-full px-3 mb-6'>
-            <label
-              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              htmlFor='email'
-            >
-              Email
-            </label>
-            <input
-              className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-              id='email'
-              type='email'
-              value={inputs.email}
-              onChange={handleChange}
-              placeholder='Email'
-              required
-            />
-          </div>
-          <div className='w-full px-3 mb-6'>
-            <label
-              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              htmlFor='subject'
-            >
-              Subject
-            </label>
-            <input
-              className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-              id='subject'
-              type='text'
-              value={inputs.subject}
-              onChange={handleChange}
-              placeholder='Subject'
-              required
-            />
-          </div>
-          <div className='w-full px-3'>
-            <label
-              className='block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2'
-              htmlFor='message'
-            >
-              Message
-            </label>
-            <textarea
-              className='appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white'
-              id='message'
-              value={inputs.message}
-              onChange={handleChange}
-              placeholder='Message'
-              required
-            ></textarea>
-          </div>
-        </div>
-        {form.state === 'error' && (
-          <p className='text-red-500'>{form.message}</p>
+    <div>
+      <form onSubmit={(e) => onSubmitForm(e)}>
+        <input
+          id='name'
+          type='text'
+          value={inputs.name}
+          onChange={handleChange}
+          placeholder='Name'
+          required
+        />
+        <input
+          id='email'
+          type='email'
+          value={inputs.email}
+          onChange={handleChange}
+          placeholder='Email'
+          required
+        />
+        <input
+          id='subject'
+          type='text'
+          value={inputs.subject}
+          onChange={handleChange}
+          placeholder='Subject'
+          required
+        />
+        <textarea
+          id='message'
+          type='text'
+          value={inputs.message}
+          onChange={handleChange}
+          placeholder='Message'
+          rows='5'
+          required
+        />
+
+        <input type='submit' />
+        {form.state === 'loading' ? (
+          <div>Sending....</div>
+        ) : form.state === 'error' ? (
+          <div>{form.message}</div>
+        ) : (
+          form.state === 'success' && <div>Sent successfully</div>
         )}
-        {form.state === 'success' && (
-          <p className='text-green-500'>{form.message}</p>
-        )}
-        <div className='flex items-center justify-center'>
-          <button
-            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
-            type='submit'
-          >
-            Send
-          </button>
-        </div>
       </form>
     </div>
   )
